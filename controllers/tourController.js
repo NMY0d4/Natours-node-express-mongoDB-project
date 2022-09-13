e9d6295c6ae9d9570b21e71449bf3520a8089544
Grégory Marini <greg.marini@hotmail.fr>
2022-09-13 20:42:34 +0200

    ajout des fonctions midleware 'checkId' et 'CheckBody' dans tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,12 +6,20 @@ const tours = JSON.parse(
 
 /// midleware param pour check l'id
 exports.checkId = (req, res, next, val) => {
-  console.log(`Tour id is : ${val}`);
-
   if (+val > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
     });
   }
   next();
@@ -44,10 +52,10 @@ exports.createTour = (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
-
   tours.push(newTour);
+
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
