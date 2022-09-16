f0102a91be4b827f968153a1b0e7aface8171def
Grégory Marini <greg.marini@hotmail.fr>
2022-09-16 17:34:39 +0200

    création premier model avec mongoose dans server

--- server.js
+++ server.js
@@ -14,10 +14,27 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then((con) => {
-    console.log(con.connections);
+  .then(() => {
     console.log('DB connection successfull!');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must havez a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const app = require('./app');
 
