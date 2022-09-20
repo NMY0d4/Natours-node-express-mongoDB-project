9ed631d61f2258da3e4f306acd41d9b186aa57bc
Grégory Marini <greg.marini@hotmail.fr>
2022-09-20 19:07:13 +0200

    ajout système de filtre dans tour controller.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,37 @@
+// const { query } = require('express');
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
 
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // { duration: { gte: '5' }, difficulty: 'easy' }
+    // { duration: { gte: '5' }, difficulty: 'easy' }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestAt: req.requestTime,
