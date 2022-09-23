8ae2294e1046fcba2a2973c1faf8b69a04c2b070
Grégory Marini <greg.marini@hotmail.fr>
2022-09-23 17:00:25 +0200

    ajout système de pagination(limite à 100) paramétrable

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,6 +30,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    console.log(page, limit, skip);
+
+    if (+req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
