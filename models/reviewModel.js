4cf9b449021f13bcfb8d64422667c95ac21c20e8
Grégory Marini <greg.marini@hotmail.fr>
2022-11-01 15:49:50 +0100

    Calculating Average Rating on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -6,11 +7,11 @@ const reviewSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Review can not be empty'],
     },
-    ratting: {
+    rating: {
       type: Number,
       min: 1,
       max: 5,
-      required: [true, 'ratting can not be empty'],
+      required: [true, 'rating can not be empty'],
     },
     createdAt: {
       type: Date,
@@ -44,6 +45,32 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
