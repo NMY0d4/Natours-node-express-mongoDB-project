4fcfc5fbea1e12f2ed61e0c9503f40480326cab1
Grégory Marini <greg.marini@hotmail.fr>
2022-11-10 17:30:34 +0100

    creation login and use axios with our api/users/login in a js file

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+// const User = require('../models/UserModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -15,18 +16,21 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  // 1) Get the data, for the requested tour (including reviews and guides)
+  // Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
 
-  // 2) Build template
-
-  // 3) Render template using data from 1
+  // Render template using data from 1
   res.status(200).render('tour', {
     title: tour.name,
-
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
-const { getOverview, getTour } = require('../controllers/viewsController');
+const {
+  getOverview,
+  getTour,
+  getLoginForm,
+} = require('../controllers/viewsController');
 
 const router = express.Router();
 
 router.get('/', getOverview);
-
 router.get('/tour/:slug', getTour);
+
+// /login
+router.get('/login', getLoginForm);
 
 module.exports = router;
