2f813e0c4b9df69c5f4ebc0be5b7e061512ac9de
Grégory Marini <greg.marini@hotmail.fr>
2022-11-14 19:12:46 +0100

    setting userdata in account page works

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,15 +3,17 @@ const {
   getOverview,
   getTour,
   getLoginForm,
+  getAccount,
+  // updateUserData,
 } = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.isLoggedIn);
-
-router.get('/', getOverview);
-router.get('/tour/:slug', getTour);
-router.get('/login', getLoginForm);
+router.get('/', authController.isLoggedIn, getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, getTour);
+router.get('/login', authController.isLoggedIn, getLoginForm);
+router.get('/me', authController.protect, getAccount);
+// router.post('/submit-user-data', authController.protect, updateUserData);
 
 module.exports = router;
