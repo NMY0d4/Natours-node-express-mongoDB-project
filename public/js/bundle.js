43f524bc1edae47a11a39a6c0ae08936180f6bf2
Grégory Marini <greg.marini@hotmail.fr>
2022-11-14 10:22:01 +0100

    review code and fix some issues and delete console.log

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -11992,9 +11992,8 @@ var login = /*#__PURE__*/function () {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log(email, password);
-            _context.prev = 1;
-            _context.next = 4;
+            _context.prev = 0;
+            _context.next = 3;
             return (0, _axios.default)({
               method: 'POST',
               url: 'http://localhost:3000/api/v1/users/login',
@@ -12003,7 +12002,7 @@ var login = /*#__PURE__*/function () {
                 password: password
               }
             });
-          case 4:
+          case 3:
             res = _context.sent;
             if (res.data.status === 'success') {
               (0, _alert.showAlert)('success', 'Logged in successfully');
@@ -12011,18 +12010,18 @@ var login = /*#__PURE__*/function () {
                 location.assign('/');
               }, 1000);
             }
-            _context.next = 11;
+            _context.next = 10;
             break;
-          case 8:
-            _context.prev = 8;
-            _context.t0 = _context["catch"](1);
+          case 7:
+            _context.prev = 7;
+            _context.t0 = _context["catch"](0);
             (0, _alert.showAlert)('error', _context.t0.response.data.message);
-          case 11:
+          case 10:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, null, [[1, 8]]);
+    }, _callee, null, [[0, 7]]);
   }));
   return function login(_x, _x2) {
     return _ref.apply(this, arguments);
@@ -12206,7 +12205,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58803" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51082" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
