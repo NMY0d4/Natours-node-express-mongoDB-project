975e51b10df3b20349a33acb75826e7c61445fbf
Grégory Marini <greg.marini@hotmail.fr>
2022-11-14 11:24:53 +0100

    add logout function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const {
   signup,
+  logout,
   login,
   forgotPassword,
   resetPassword,
@@ -15,6 +16,7 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/logout', logout);
 
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
