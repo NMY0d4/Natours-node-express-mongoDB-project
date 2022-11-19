4e9984d847b9fda6a4b170d135161c2be752c43c
Grégory Marini <greg.marini@hotmail.fr>
2022-11-19 17:59:38 +0100

    fihishing booking crud api route and started building signup view

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const bookingController = require('../controllers/bookingController');
-const { protect } = require('../controllers/authController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.use(protect);
 
 router.get(
   '/checkout-session/:tourId',
   protect,
   bookingController.getCheckoutSession
 );
+
+router.use(restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
