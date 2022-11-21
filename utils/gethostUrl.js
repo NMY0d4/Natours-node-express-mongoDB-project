eaab2ee8887f884ba366e14b72d9a467b726b808
Grégory Marini <greg.marini@hotmail.fr>
2022-11-21 11:57:54 +0100

    try to fix bug heroku, port='undefined'

--- utils/gethostUrl.js
+++ utils/gethostUrl.js
@@ -1,4 +1,4 @@
 module.exports = (req) =>
-  req.get('host') === `127.0.0.1:${process.env.port}`
+  req.get('host') === `127.0.0.1:${process.env.PORT}`
     ? `localhost:3000`
     : req.get('host');
