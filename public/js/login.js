98395424e38c9185a2de0ad672be700fd3dfc93a
Grégory Marini <greg.marini@hotmail.fr>
2022-11-22 11:04:52 +0100

    full code review, saw 2 oversights corrected, new deployment test

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ export const logout = async () => {
     });
 
     if (res.data.status === 'success') {
-      location.reload(true);
+      location.assign('/');
     }
   } catch (err) {
     console.log(err.response);
