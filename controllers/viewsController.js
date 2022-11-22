041623cfc69db321042af7f58150ca0f46813f24
Grégory Marini <greg.marini@hotmail.fr>
2022-11-22 09:50:54 +0100

    test desactived stripe

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -7,14 +7,14 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) get tour data from collection
-  // const tours = await Tour.find();
+  const tours = await Tour.find();
 
   // 2) build template
 
   // Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
-    // tours,
+    tours,
   });
 });
 
